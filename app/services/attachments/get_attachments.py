a598978e294df093d55b1f14ecb073a99aff80f4
FrankDannie <frank.dannie@gmail.com>
2024-08-14 11:49:13 +0900

    Update attachments API

--- app/services/attachments/get_attachments.py
+++ app/services/attachments/get_attachments.py
@@ -2,6 +2,6 @@ from sqlalchemy.orm import Session
 from app.crud.attachments.get_attachments import get_attachment
 from app.schemas.attachments.get_attachments import AttachmentOut
 
-def fetch_attachment(db: Session, attachment_id: int) -> AttachmentOut:
-    attachment = get_attachment(db, attachment_id)
+def fetch_attachment(db: Session, projectId: int, taskId: int) -> AttachmentOut:
+    attachment = get_attachment(db, projectId, taskId)
     return AttachmentOut.from_orm(attachment)
